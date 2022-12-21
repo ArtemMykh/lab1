0ab2a42dede1bccda615efc8ab99b20ac5fcd95e
ArtemMykh <artem192837465@gmail.com>
2022-12-21 19:42:11 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
-}
+  posts: Post[] = [
+    {title:"Zieit", text: "Zaporizhzhia Institute of Economics and Information Technologies (ZIEIT) is a private institute of higher education in the city of Zaporizhzhia, Ukraine, with branches in the cities of Melitopol and Kryvyi Rih.", id: 1},
+    {title:"React", text: "JavaScript-библиотека для создания пользовательских интерфейсов", id: 2},
+    {title:"Angular", text: "Angular is an app-design framework and development platform for creating efficient and sophisticated single-page apps.", id: 3},
+    {title:"Vue", text: "Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces", id: 4},
+    {title:"Node.js", text: "Node.js® — це JavaScript–оточення побудоване на JavaScript–рушієві Chrome V8.", id: 5}
+  ]
+  }
+export  interface  Post {
+  title: string
+  text: string
+  id?: number
+  }
